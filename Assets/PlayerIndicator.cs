cae663c617688641e1f1b712755ac23102cc9685
THE_BA_MACHINE\Ben <bcalbert777@gmail.com>
2018-04-09 07:22:12 -0400

    Created an indicator for who you are playing

---
+++ Assets/PlayerIndicator.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerIndicator : MonoBehaviour {
+
+    public Sprite HolmesSprtie;
+    public Sprite MoriartySprite;
+
+    private PlayerType MyPlayerType = PlayerType.Holmes;
+
+	// Use this for initialization
+	void Start () {
+        if (FindObjectOfType<LevelPropertyManager>() != null)
+        {
+            MyPlayerType = FindObjectOfType<LevelPropertyManager>().GetPlayerType();
+        }
+
+        switch (MyPlayerType)
+        {
+            case PlayerType.Holmes:
+                GetComponentInChildren<Image>().sprite = HolmesSprtie;
+                break;
+            case PlayerType.Moriarty:
+                GetComponentInChildren<Image>().sprite = MoriartySprite;
+                break;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
