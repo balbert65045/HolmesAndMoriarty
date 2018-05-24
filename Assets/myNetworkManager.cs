b1edb76edaf25c4c530a517b85837701947df25c
THE_BA_MACHINE\Ben <bcalbert777@gmail.com>
2018-05-24 06:53:28 -0400

    Added NetworkManager to Start screen

---
+++ Assets/myNetworkManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class myNetworkManager : NetworkManager {
+
+    public void MyStartHost()
+    {
+        StartHost();
+        Debug.Log(Time.timeSinceLevelLoad + " Host started");
+    }
+
+    public override void OnStartHost()
+    {
+        base.OnStartHost();
+
+        //DoorLocations doorlocals = FindObjectOfType<DoorLocations>();
+        //GameObject door = Instantiate(spawnPrefabs[1], doorlocals.doorPositions[0].position, doorlocals.doorPositions[0].rotation);
+        //NetworkServer.Spawn(door);
+
+        Debug.Log(Time.timeSinceLevelLoad + " Host requested");
+    }
+
+   
+
+    public override void OnStartClient(NetworkClient myClient)
+    {
+        base.OnStopClient();
+        Debug.Log(Time.timeSinceLevelLoad + " Client start requested" );
+    }
+
+    public override void OnClientConnect(NetworkConnection conn)
+    {
+        base.OnClientConnect(conn);
+        Debug.Log(Time.timeSinceLevelLoad + " Client connected to IP:" + conn.address);
+        Debug.Log(Network.player.ipAddress);
+
+
+    }
+}
