5ee1b24a2e3f945391307367f06b5423a1c4cc5e
THE_BA_MACHINE\Ben <bcalbert777@gmail.com>
2018-06-08 07:32:52 -0400

    Fixx

--- Assets/LevelPropertyManagerMulti.cs
+++ Assets/LevelPropertyManagerMulti.cs
@@ -24,37 +24,63 @@ public class LevelPropertyManagerMulti : MonoBehaviour {
         if (Player1Type == PlayerType.Holmes && Player2Type == PlayerType.Holmes)
         {
             int PlayerID = Random.Range(1, 3);
-            if (PlayerID == 1) { Player1Player = PlayerType.Moriarty; }
-            else if (PlayerID == 2) { Player2Player = PlayerType.Moriarty; }
+            if (PlayerID == 1) {
+                P1Moriarty_P2Holmes();
+            }
+            else if (PlayerID == 2) {
+                P1Holmes_P2Moriarty();
+            }
         }
         else if (Player1Type == PlayerType.Moriarty && Player2Type == PlayerType.Moriarty)
         {
             int PlayerID = Random.Range(1, 3);
-            if (PlayerID == 1) { Player1Player = PlayerType.Holmes; }
-            else if (PlayerID == 2) { Player2Player = PlayerType.Holmes; }
+            if (PlayerID == 1) {
+                P1Holmes_P2Moriarty();
+            }
+            else if (PlayerID == 2) {
+                P1Moriarty_P2Holmes();
+            }
         }
         else if (Player1Type == PlayerType.Random && Player2Type == PlayerType.Random)
         {
             int PlayerID = Random.Range(1, 3);
             if (PlayerID == 1) {
-                Player1Player = PlayerType.Holmes;
-                Player2Player = PlayerType.Moriarty;
+                P1Holmes_P2Moriarty();
             }
             else if (PlayerID == 2) {
-                Player2Player = PlayerType.Holmes;
-                Player1Player = PlayerType.Moriarty;
+                P1Moriarty_P2Holmes();
             }
         }
         else if (Player1Type == PlayerType.Random)
         {
-            if (Player2Type == PlayerType.Holmes) { Player1Player = PlayerType.Moriarty; }
-            if (Player2Type == PlayerType.Moriarty) { Player1Player = PlayerType.Holmes; }
+            if (Player2Type == PlayerType.Holmes) {
+                P1Moriarty_P2Holmes();
+            }
+            else if (Player2Type == PlayerType.Moriarty) {
+                P1Holmes_P2Moriarty();
+            }
         }
         else if (Player2Type == PlayerType.Random)
         {
-            if (Player1Type == PlayerType.Holmes) { Player2Player = PlayerType.Moriarty; }
-            if (Player1Type == PlayerType.Moriarty) { Player2Player = PlayerType.Holmes; }
+            if (Player1Type == PlayerType.Holmes) {
+                P1Holmes_P2Moriarty();
+            }
+            else if (Player1Type == PlayerType.Moriarty) {
+                P1Moriarty_P2Holmes();
+            }
         }
+    }
+
+    void P1Holmes_P2Moriarty()
+    {
+        Player1Player = PlayerType.Holmes;
+        Player2Player = PlayerType.Moriarty;
+    }
+
+    void P1Moriarty_P2Holmes()
+    {
+        Player1Player = PlayerType.Moriarty;
+        Player2Player = PlayerType.Holmes;
     }
 
      // Use this for initialization
